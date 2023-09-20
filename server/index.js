6626045fd27cb19c653c739fdfce386dfecba3a0
Utkarsh Srivastava <utkarsh.dev83@gmail.com>
2023-09-20 16:57:12 +0530

    updated domain url

--- server/index.js
+++ server/index.js
@@ -14,8 +14,16 @@ const app = express();
 //app.use is used to handle any type of requests or method 
 //app.use(path,handler function)
 
-app.use(cors())
 
+// app.use(cors())
+const HOST = process.env.HOST;
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://create-your-space-serverside.vercel.app"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions)); // Use cors with the configured options
 //to handle invalid chars in url 
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
@@ -23,16 +31,14 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.use('/', router)
 
-app.get("/",(req,res)=>{
+app.get('/',(req,res)=>{
 
     res.send("Hello user ")
 
 })
-app.get("/login", router)
 
 
 const PORT = 8000;
-
 const USERNAME = process.env.DB_USERNAME
 
 const PASSWORD = process.env.DB_PASSWORD
@@ -40,8 +46,14 @@ const PASSWORD = process.env.DB_PASSWORD
 //parse the importted usrname and password
 Connection(USERNAME, PASSWORD);
 
-app.listen(PORT,() =>{
+//'0.0.0.0' will make your Express server will listen on all available network interfaces, making it accessible externally. 
+app.listen(PORT,'0.0.0.0',() =>{
 
     console.log(`server is running on port: ${PORT}`)
 
 });
+
+
+// app.listen( () => {
+//     console.log("Server is running");
+// })
